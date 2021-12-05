8aa8ece363d3b57fdff5cdb71c361a75fddb6b58
AugustoComeglio <augustoc.98@gmail.com>
2021-12-05 04:03:18 -0300

    Cambios Email

---
+++ php/enviar.php
@@ -0,0 +1,19 @@
+<?php
+    $destino = "augustoc.98@gmail.com";
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $telefono = $_POST["telefono"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " . $telefono . "\nMensaje: " . $mensaje;
+
+
+    mail($destino,"Mensaje de pagina web", $contenido);
+    header("Location:gracias.html");
+
+
+
+
+
+
+
+?>
