b4a54a0ad27017ab0bcfa04351e067902d52e708
AugustoComeglio <augustoc.98@gmail.com>
2021-12-05 20:39:18 -0300

    Sobre mi avances

---
+++ JavaScrip/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    //para que se abra con el primer enlace abierto
+    $('ul.tabs li a:first').addClass('active');
+
+    //llamamos a todos los articulos y los ocultamos
+	$('.secciones article').hide();
+
+    //llamamos los articulos y mostramos solo el primero
+	$('.secciones article:first').show();
+
+    //cuando damos click remueve clase activo y se la pone al que le damos click para mostrarlo
+	$('ul.tabs li a').click(function(){
+		$('ul.tabs li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+        //para que apararezca el contenido del que activamos
+		var activeTab = $(this).attr('href');
+		$(activeTab).show();
+		return false;
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
